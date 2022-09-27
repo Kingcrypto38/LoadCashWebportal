4b9ddf5bc634d2d1e00222c3be15cb4e274c01de
Arsalanur Rahman <arsalanur.rahman@cognizant.com>
2022-09-27 13:25:21 +0530

    Initial commit

---
+++ src/app/Components/outstanding-balance/outstanding-balance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outstanding-balance',
+  templateUrl: './outstanding-balance.component.html',
+  styleUrls: ['./outstanding-balance.component.css']
+})
+export class OutstandingBalanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CashComponent } from './Components/cash/cash.component';
+import { UploadComponent } from './Components/upload/upload.component';
+import { OutstandingBalanceComponent } from './Components/outstanding-balance/outstanding-balance.component';
+import { TopNavComponent } from './Components/top-nav/top-nav.component';
+import { SideNavComponent } from './Components/side-nav/side-nav.component';
+import { ModalComponent } from './Components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    OutstandingBalanceComponent,
+    TopNavComponent,
+    SideNavComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
