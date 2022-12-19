04fc57b4f029e3df091fc1d952e29c524809330d
Arsalanur Rahman <arsalanur.rahman@cognizant.com>
2022-12-19 23:44:33 +0530

    Caching + Dummy Table + One backend node connected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CashComponent } from './Components/cash/cash.component';
+import { UploadComponent } from './Components/upload/upload.component';
 
 const routes: Routes = [
- { path:"cash", component: CashComponent }
+ { path:"cash", component: CashComponent },
+ { path:"upload", component: UploadComponent }
 ];
 
 @NgModule({
